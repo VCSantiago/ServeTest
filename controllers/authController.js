da1111b9cf6e2563bc9c40fb0fdf5c34e5dc9850
vini0707 <vinicius.csantiago@hotmail.com>
2019-09-19 20:31:31 -0300

    teste

--- controllers/authController.js
+++ controllers/authController.js
@@ -3,7 +3,7 @@ const bcrypt = require('bcryptjs');
 const router = express.Router();
 const jwt = require('jsonwebtoken');
 const authConfig = require('../config/auth');
-const User = require('../models/User');
+const User = require('../models/user');
 
 function geraToken(params = {}){
     return jwt.sign(params, authConfig.secret, {
