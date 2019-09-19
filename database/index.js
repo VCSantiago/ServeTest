ff5e5fca909cb6c2c9ae52e88b697d6e804d93b6
vini0707 <vinicius.csantiago@hotmail.com>
2019-09-19 17:17:52 -0300

    banco

--- database/index.js
+++ database/index.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/agoravai', {useUnifiedTopology: true,
+mongoose.connect('mongodb+srv://santiago:santiago@cluster0-swqjv.mongodb.net/test?retryWrites=true&w=majority', {useUnifiedTopology: true,
 useNewUrlParser: true});
 mongoose.set('useCreateIndex', true);
 mongoose.Promise = global.Promise;
